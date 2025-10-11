bbd638172de46532ff39b2b9d0c1b584eaf1e7fa
Chris <chrisbreuer93@gmail.com>
2025-10-11 11:01:00 -0700

    chore: wip

--- packages/pickier/build.ts
+++ packages/pickier/build.ts
@@ -3,7 +3,6 @@ import { dts } from 'bun-plugin-dtsx'
 await Bun.build({
   entrypoints: ['src/index.ts', 'bin/cli.ts'],
   outdir: './dist',
-  splitting: true,
   target: 'bun',
   plugins: [dts()],
 })
